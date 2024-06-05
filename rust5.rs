ee1f102822312cb4f425c82ede169ef7342c1821
Abdullah Abdulwaheed <81182079+Habfrejo@users.noreply.github.com>
2024-06-05 13:10:16 +0100

    Create rust5.rs
    rust5.rs

---
+++ rust5.rs
@@ -0,0 +1,15 @@
+//Day 4,5th June 2024
+//Shadowing
+You can declare a new variable with the same name as a previous variable here we can say ** the first variable is shadowed by the second one
+
+//only modify `assert_eq!`  to make the `println! ` work(print `42` in terminnal)
+let x: i32 = 5;
+{
+ let x = 12;
+ assert_eq!(x,5);
+ 
+}
+assert_eq(x,5);
+
+let x = 42;
+println!("{}", x); //Prints "42".
