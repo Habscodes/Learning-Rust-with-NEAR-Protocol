d9bdc5423a081c6f7704c0de65d7a3ee9d8832ca
Abdullah Abdulwaheed <81182079+Habfrejo@users.noreply.github.com>
2024-06-05 13:14:52 +0100

    Update rust5.rs
    I'll make some changes later

--- rust5.rs
+++ rust5.rs
@@ -3,15 +3,15 @@
 // You can declare a new variable with the same name as a previous variable here we can say ** the first variable is shadowed by the second one
 
 //only modify `assert_eq!`  to make the `println! ` work(print `42` in terminnal)
- fn main(){
+ // fn main(){
 let x: i32 = 5;
 {
  let x = 12;
  assert_eq!(x,5);
  
 }
-assert_eq(x,5);
+assert_eq!(x,5);
 
 let x = 42;
 println!("{}", x); //Prints "42".
-}
+// }
